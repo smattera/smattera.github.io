905015faeade2250d06e6b2aae69ebe8ae0049f9
Sonny Mattera <smattera@hotmail.com>
2021-09-08 10:07:13 -0500

    update all 2

---
+++ timer.js
@@ -0,0 +1,45 @@
+// Set the date we're counting down to
+var countDownDate = new Date("July 14, 2022 10:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  if (seconds < 10) {
+    seconds = "0" + seconds;
+  }
+
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+
+  if (hours < 10) {
+    hours = "0" + hours;
+  }
+
+  if (days < 10) {
+    days = "0" + days;
+  }
+
+
+  // Display the result in the element with id="demo"
+  document.getElementById("timer").innerHTML = days + " : " + hours + " : "
+  + minutes + " : " + seconds;
+
+  // If the count down is finished, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("timer").innerHTML = "The 72nd Annual Choctaw Indian Fair has begun!";
+  }
+}, 1000);
