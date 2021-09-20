b2a95ff8fb1e834293954ba1f51cedd7b09f6463
Sonny Mattera <smattera@hotmail.com>
2021-09-20 16:25:21 -0500

    Update event-carousel.js

--- assets/js/event-carousel.js
+++ assets/js/event-carousel.js
@@ -3,7 +3,7 @@ let events = document.querySelectorAll('.carousel-event .carousel-event-item')
 events.forEach((el) => {
     const minPerSlide = 4
     let nextEvent = el.nextElementSibling
-    for (var j=1; j<minPerSlide; i++) {
+    for (var j=1; j<minPerSlide; j++) {
         if (!nextEvent) {
             // wrap carousel by using first child
         	nextEvent = events[0]
