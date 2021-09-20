5c67cab781e1e5400d9121dae14ae7823e42681d
Sonny Mattera <smattera@hotmail.com>
2021-09-20 15:55:51 -0500

    Update sponsors.html

--- assets/js/sponsor-carousel.js
+++ assets/js/sponsor-carousel.js
@@ -1,4 +1,4 @@
-let items = document.querySelectorAll('.carousel .carousel-item')
+let items = document.querySelectorAll('.carousel-sponsor .carousel-sponsor-item')
 
 items.forEach((el) => {
     const minPerSlide = 4
