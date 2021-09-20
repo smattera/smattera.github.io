1cc12801fe19f422a08692017df98521e19599fc
Sonny Mattera <smattera@hotmail.com>
2021-09-20 13:32:28 -0500

    Update events.html

--- assets/js/event-carousel.js
+++ assets/js/event-carousel.js
@@ -1,4 +1,4 @@
-let items = document.querySelectorAll('.carousel-event .carousel-event-item')
+let items = document.querySelectorAll('.carousel-events .carousel-events-item')
 
 items.forEach((el) => {
     const minPerSlide = 4
