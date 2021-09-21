50c7a2640f11d860c8c0b3d2d1552ec64563b909
Sonny Mattera <smattera@hotmail.com>
2021-09-21 12:35:47 -0500

    Update footer.html and events.html

--- assets/js/bt5carousel.js
+++ assets/js/bt5carousel.js
@@ -1,31 +1,15 @@
-let items = document.querySelectorAll('.carousel-sponsor .carousel-sponsor-item')
+let items = document.querySelectorAll('.carousel .carousel-item')
 
 items.forEach((el) => {
     const minPerSlide = 4
-    let nextSponsor = el.nextElementSibling
+    let next = el.nextElementSibling
     for (var i=1; i<minPerSlide; i++) {
-        if (!nextSponsor) {
+        if (!next) {
             // wrap carousel by using first child
-        	nextSponsor = items[0]
+        	next = items[0]
       	}
-        let cloneSponsorChild = next.cloneNode(true)
-        el.appendChild(cloneSponsorChild.children[0])
-        nextSponsor = nextSponsor.nextElementSibling
-    }
-})
-
-let itemsEvent = document.querySelectorAll('.carousel-event .carousel-event-item')
-
-itemsEvent.forEach((elEvent) => {
-    const minPerSlide = 4
-    let nextEvent = elEvent.nextElementSibling
-    for (var j=1; j<minPerSlide; j++) {
-        if (!nextEvent) {
-            // wrap carousel by using first child
-        	nextEvent = itemsEvent[0]
-      	}
-        let cloneEventChild = nextEvent.cloneNode(true)
-        elEvent.appendChild(cloneEventChild.children[0])
-        nextEvent = nextEvent.nextElementSibling
+        let cloneChild = next.cloneNode(true)
+        el.appendChild(cloneChild.children[0])
+        next = next.nextElementSibling
     }
 })
