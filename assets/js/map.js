46e672a98e658eadd58563b9a5d371e7c1d4e480
Sonny Mattera <smattera@hotmail.com>
2022-04-12 14:53:26 -0500

    Update map.js

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,14 +1,24 @@
 let map;
 
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
+  const map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 32.77929, lng: -89.22484},
     zoom: 15
   });
 
   marker = new google.maps.Marker({
     map,
+    draggable: false,
     animation: google.maps.Animation.DROP,
     position: { lat: 59.327, lng: 18.067 },
   });
+  marker.addListener("click", toggleBounce);
+}
+
+function toggleBounce() {
+  if (marker.getAnimation() !== null) {
+    marker.setAnimation(null);
+  } else {
+    marker.setAnimation(google.maps.Animation.BOUNCE);
+  }
 }
